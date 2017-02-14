d54c7c82af63b83bf4dcaa14b5f25dbb0c919d53
administrator <administrator@C02RX2E5FVH6.netgear.com>
2017-02-14 14:25:44 -0500

    refactored for readability

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,12 +2,12 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i < 0) {
-            throw new IllegalArgumentException("Input cannot be negative.");
+        if (i < 0) throw new IllegalArgumentException("Input cannot be negative.");
+
+        int factorial = 1;
+        for (int j = 1, jj = i; j <= jj; j++) {
+            factorial *= j;
         }
-        for (int j = 1, jj = i; j < jj; j++) {
-            i *= j;
-        }
-        return Math.max(1,i);
+        return factorial;
     }
 }
