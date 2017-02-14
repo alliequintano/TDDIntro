aa774e303401f64c904884c58d5c5de385eef408
administrator <administrator@C02RX2E5FVH6.netgear.com>
2017-02-14 14:21:33 -0500

    throws IllegalArgumentException if input is negative

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("Input cannot be negative.");
+        }
         for (int j = 1, jj = i; j < jj; j++) {
             i *= j;
         }
