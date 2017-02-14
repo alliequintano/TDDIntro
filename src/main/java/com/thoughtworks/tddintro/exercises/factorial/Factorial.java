92bb3e33687a40b1885c68af80866f51f9a507a7
administrator <administrator@C02RX2E5FVH6.netgear.com>
2017-02-14 14:15:39 -0500

    returns 6 when input is 3

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        int k = i;
+        for (int j = 1; j < k; j++) {
+            i *= j;
+        }
         return Math.max(1,i);
     }
 }
