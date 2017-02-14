67e43a794519209f6ef5054f9ba89dff29948528
administrator <administrator@C02RX2E5FVH6.netgear.com>
2017-02-14 14:17:23 -0500

    refactored for loop

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,8 +2,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        int k = i;
-        for (int j = 1; j < k; j++) {
+        for (int j = 1, jj = i; j < jj; j++) {
             i *= j;
         }
         return Math.max(1,i);
